3b34c1ee9ec43ff58a9d6702f9958666f5a46662
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2020-05-29 15:55:50 +0300

    upgrade a new files

--- js/main.js
+++ js/main.js
@@ -15,6 +15,30 @@ $(function(){
         destination = $(elementClick).offset().top;
         $("body,html").animate({scrollTop: destination }, 800);
     });
+    $("#btn").click(
+		function(){
+			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+			return false; 
+		}
+    );
+    $('.consultation__button').click(function(){
+        $('#exampleModal1').arcticmodal();
+    })
+    function sendAjaxForm(result_form, ajax_form, url) {
+        $.ajax({
+            url:    "action_ajax_form.php",
+            type:     "POST", 
+            dataType: "html", 
+            data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+            success: function(response) { //Данные отправлены успешно
+                result = $.parseJSON(response);
+                $('#result_form').html('Имя: '+result.name+' <br>Email: '+result.email+' <br>Телефон: '+result.phonenumber);
+            },
+            error: function(response) { // Данные не отправлены
+                $('#result_form').html('Ошибка. Данные не отправлены.');
+            }
+        })
+    }
 })
 
    
